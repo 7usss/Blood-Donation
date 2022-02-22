c8edd97f4fac0961baa389a5e0a370eb6b9ce473
hussam <hasooooomi52@gmail.com>
2022-02-22 15:21:24 +0300

    add register system and  activity_main.xml

--- app/src/main/java/com/example/myapplicationautho/Login.kt
+++ app/src/main/java/com/example/myapplicationautho/Login.kt
@@ -4,19 +4,20 @@ package com.example.myapplicationautho
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
-
-var GoToRegisterPage:TextView?= null
-var Emailinloin:EditText?= null
-
 class Login() : AppCompatActivity()  {
+    private var GoToRegisterPage:TextView?= null
+    private var Emailinloin:EditText?= null
+    private var Passwordinlogin:EditText?= null
+    private var buttoninlogin :Button?= null
+    private var forgotpassword :TextView?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
-
         connectview()
         movetoregisterpage()
     }
@@ -31,6 +32,9 @@ class Login() : AppCompatActivity()  {
     private fun connectview() {
         GoToRegisterPage=findViewById(R.id.goToregisterpageTV)
         Emailinloin = findViewById(R.id.emailTV)
+        Passwordinlogin = findViewById(R.id.passwordET)
+        buttoninlogin = findViewById(R.id.buttonLogin)
+        forgotpassword = findViewById(R.id.forgotpasswordTV)
     }
 
 
--- app/src/main/java/com/example/myapplicationautho/register.kt
+++ app/src/main/java/com/example/myapplicationautho/register.kt
@@ -3,17 +3,55 @@ package com.example.myapplicationautho
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
-
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class register : AppCompatActivity() {
+
+    private var Nameinregister:EditText? =null
+    private var emailinregister:EditText? =null
+    private var passwordinregister:EditText? =null
+    private var phoneinregister:EditText? =null
+    private var buttoninregister:Button? =null
+    private var Authe:FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
         connectview()
+        Authe= FirebaseAuth.getInstance()
+        newaccount()
 
     }
-    private fun connectview() {
 
+    private fun newaccount() {
+        buttoninregister?.setOnClickListener {
+            var name=Nameinregister?.text.toString().trim()
+            var email= emailinregister?.text.toString().trim()
+            var password = passwordinregister?.text.toString().trim()
+            var phone = phoneinregister?.text?.trim()
+            Authe?.createUserWithEmailAndPassword(email,password)?.addOnCompleteListener {
+
+                if (it.isSuccessful){
+                    var move_to_main_activity:Intent= Intent(this,MainActivity::class.java)
+                    startActivity(move_to_main_activity)
+                    finish()
+                }else{
+                    Toast.makeText(this, it.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
+
+        }
+    }
+
+    private fun connectview() {
+        Nameinregister = findViewById(R.id.Name)
+        emailinregister = findViewById(R.id.email)
+        passwordinregister = findViewById(R.id.pass)
+        phoneinregister = findViewById(R.id.phone)
+        buttoninregister = findViewById(R.id.buttonRegister)
     }
 }
