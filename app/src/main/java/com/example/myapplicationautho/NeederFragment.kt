c5336fd1e8427e335d93b9af154cfe7eaef3d795
ahmad88a88 <101225362+ahmad88a88@users.noreply.github.com>
2022-03-24 20:27:00 +0300

    insert photo and edit on NeederFragment.kt

--- app/src/main/java/com/example/myapplicationautho/NeederFragment.kt
+++ app/src/main/java/com/example/myapplicationautho/NeederFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 
 
 class NeederFragment : Fragment() {
+
+    private var RecyclerView:RecyclerView? =null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,7 +18,17 @@ class NeederFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_needer, container, false)
+
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        connectViews(view)
+    }
+
+    private fun connectViews(view: View){
+        RecyclerView = view.findViewById(R.id.RecyclerView)
+    }
 
 }
